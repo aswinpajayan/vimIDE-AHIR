961069e5ff6ec0d057a03f38b36d4e05454c75a7
Aswin P Ajayan <aswin@ee.iitb.ac.in>
2019-09-12 13:07:40 +0530

    To install ghdl with gcc backend

---
+++ install_ghdl_gcc_backend.sh
@@ -0,0 +1,19 @@
+#this file is taken from https://github.com/ymahajan456/Tutorials/tree/master/VHDL_Tutorial/Installation
+#visit the link for more information and simple tutorials on vhdl,verilog 
+echo "cloned from https://github.com/ymahajan456/Tutorials/tree/master/VHDL_Tutorial/Installation"
+sudo add-apt-repository ppa:pgavin/ghdl
+sudo apt-get update
+sudo apt-get install ghdl
+
+sudo apt-get install zip unzip
+
+echo Installing Other Packages
+sudo apt-get install gtkwave xterm gnuplot
+
+echo Installation Ended. Please check the output on the terminal for any errors.
+echo Running Checks. If the following commands give any error then the installation was not successful.
+ghdl --help
+ghdl --version
+
+#echo
+#echo Check if the Installation is successful by running \"ghdl --help\" or \"ghdl --version\"
